0e63a13894c99866e17318ff67cf1adbdf715c90
Sebastian Kraft <skraft@hsu-hh.de>
2014-09-11 14:21:34 +0200

    improved PHP web service script

--- web_service/beaqleJS_Service.php
+++ web_service/beaqleJS_Service.php
@@ -7,10 +7,13 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: POST, GET");
     header('Content-type: application/json');
-    
+
+    //error_reporting(-1);
+    //ini_set('display_errors', true);
+
     // subfolder for results, folder has to exist and needs proper write permissions
-    $results_prefix = "results/";
-    
+    $results_prefix = "./results/";
+
     // check if necessary data is available
     if (isset($_POST['testresults'])) {
     
@@ -31,11 +34,11 @@
     	$filename = $filename."$filenumber".".txt";
     	
 	    // write the file
-	    $err1 = file_put_contents($results_prefix.$filename, print_r($testresults, TRUE), FILE_APPEND);
+	    $err1 = file_put_contents($results_prefix.$filename, print_r($testresults, TRUE));
 	
 	    if ($err1===false) {
             $return['error'] = true;
-            $return['message'] = "Error writing data!";    
+            $return['message'] = "Error writing data to file! (".$results_prefix.$filename.")";    
 	    } else {
             $return['error'] = false;
             $return['message'] = "Data is saved!";    

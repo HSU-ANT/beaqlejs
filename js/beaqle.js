f35da579e185c0e9cdb808e6c9b7357a23e49a50
Sebastian Kraft <skraft@hsu-hh.de>
2015-03-03 13:22:42 +0100

    Complete rewrite of the playback start/stop and loop code. Fade in/out is enabled again with Chrome, Safari and Opera. Firefox is still buggy...

--- js/beaqle.js
+++ js/beaqle.js
@@ -27,11 +27,12 @@
         this.NumPlayers = 0;
         this.NumUsed = 0;
         this.LoopAudio = 0;
+        this.LoopFade = false;
         this.ABPos = [0, 100];
         this.PoolID = PoolID;
         this.IDPlaying = -1;
-        this.fadeOutTime = 0.02;
-        this.positionUpdateInterval = 0.01;
+        this.fadeOutTime = 0.03;
+        this.positionUpdateInterval = 0.005;
 
         // web audio is only supported for same origin
         switch(window.location.protocol) {
@@ -52,11 +53,12 @@
              break;
         }
         
-        if (!clientIsChrome())
+	    // only enable webAudio on Chrome, Safri and Opera.
+        if (!(clientIsChrome() || clientIsSafari() || clientIsOpera()))
             this.waContext = false;
 
         // set to false to manually disable WebAudioAPI support
-        this.waContext = false;
+        //this.waContext = false;
 
         // setup regular callback timer to check current playback position
         var _this = this;
@@ -78,21 +80,14 @@
             
             // calculate progress including a look ahead for fade out or loop
             var progress = 0;
-            progress = (audiotag.currentTime+_this.positionUpdateInterval+_this.fadeOutTime) / audiotag.duration * 100;
+            progress = (audiotag.currentTime+_this.positionUpdateInterval+_this.fadeOutTime) / audiotag.duration * 100.0;
             
             // if end is reached ...
-            if (progress >= _this.ABPos[1]) {
-                if (_this.waContext!==false) {
-                    if (_this.LoopAudio == true) {
-                        var currID = _this.IDPlaying;
-                        setTimeout( function(){_this.play(currID)}, _this.fadeOutTime*1000 + 10);                    
-                    }
-                    _this.pause();
+            if ((progress >= _this.ABPos[1]) && (!_this.LoopFade)) {
+                if (_this.LoopAudio == true) {
+                    _this.loopReturn();
                 } else {
-                    if (_this.LoopAudio == true)
-                        _this.play(_this.IDPlaying);
-                    else 
-                        _this.pause();
+                    _this.pause();
                 }
             }
         }
@@ -139,10 +134,11 @@
 
         if (this.waContext!==false) {
             var gainNode = this.waContext.createGain();
+            gainNode.value = 0.00000001;
             var source = this.waContext.createMediaElementSource(audiotag);
             source.connect(gainNode);
             gainNode.connect(this.waContext.destination);
-            gainNode.gain.setValueAtTime(0.00001, 0);
+            gainNode.gain.setValueAtTime(0.0000001, 0);
             this.gainNodes[ID] = gainNode;
         }
         
@@ -167,41 +163,70 @@
     
     // play audio with specified ID
     AudioPool.prototype.play = function(ID){
-        var audiotag = $('#'+this.PoolID+' > #audio'+ID).get(0);
-        
+        var audiotag = $('#'+this.PoolID+' > #audio'+ID).get(0);        
         audiotag.currentTime = 0.000001 + this.ABPos[0] / 100.0 * audiotag.duration;
-        
+
         if (this.waContext!==false) {
-            //this.gainNodes[ID].gain.cancelScheduledValues(this.waContext.currentTime);
-            this.gainNodes[ID].gain.setValueAtTime(0.00001, this.waContext.currentTime);
-            this.gainNodes[ID].gain.exponentialRampToValueAtTime(1, this.waContext.currentTime + this.fadeOutTime);
+            var loopLen = (this.ABPos[1] - this.ABPos[0]) / 100.0 * audiotag.duration;
+            if (loopLen > this.fadeOutTime*2 + this.positionUpdateInterval*2) {
+                this.gainNodes[ID].gain.cancelScheduledValues(this.waContext.currentTime);
+                this.gainNodes[ID].gain.setTargetAtTime(1, this.waContext.currentTime, this.fadeOutTime/1.0 );
+                this.LoopFade = false;
+                audiotag.play();
+            }
+        } else {
+            audiotag.play();
         }
-    
-        audiotag.play();
+        
         this.IDPlaying = ID;
     }
+
+    // return to loop begin
+    AudioPool.prototype.loopReturn = function() { 
+
+        if (this.waContext!==false) {
+            // fade out
+            this.gainNodes[this.IDPlaying].gain.cancelScheduledValues(this.waContext.currentTime);
+            this.gainNodes[this.IDPlaying].gain.setTargetAtTime(0, this.waContext.currentTime, this.fadeOutTime/8.0 );
+            this.LoopFade = true;
+
+            var audiotag = $('#'+this.PoolID+' > #audio'+this.IDPlaying).get(0);
+            var currID = this.IDPlaying;
+            var _this  = this;
+            // wait till fade out is done
+            setTimeout( function(){
+                    _this.LoopFade = false;
+                    audiotag.currentTime = 0.000001 + _this.ABPos[0] / 100.0 * audiotag.duration;
+                    _this.gainNodes[_this.IDPlaying].gain.cancelScheduledValues(_this.waContext.currentTime);       
+                    _this.gainNodes[_this.IDPlaying].gain.setTargetAtTime(1, _this.waContext.currentTime, _this.fadeOutTime/1.0 );
+                }, 
+                this.fadeOutTime*1000 + 2
+            );            
+        } else {
+            // return to the start marker
+            var audiotag = $('#'+this.PoolID+' > #audio'+this.IDPlaying).get(0);
+            audiotag.currentTime = 0.000001 + this.ABPos[0] / 100.0 * audiotag.duration;
+            audiotag.play();
+        }
+    }
     
-    // pause currentyl playing audio
+    // pause currently playing audio
     AudioPool.prototype.pause = function() {   
 
         if (this.IDPlaying!==-1) {
-
+ 
             var audiotag = $('#'+this.PoolID+' > #audio'+this.IDPlaying).get(0);
-            if (this.waContext!==false) {
-                //this.gainNodes[this.IDPlaying].gain.cancelScheduledValues(this.waContext.currentTime);
-                this.gainNodes[this.IDPlaying].gain.setValueAtTime(1, this.waContext.currentTime);
-                this.gainNodes[this.IDPlaying].gain.exponentialRampToValueAtTime(0.00001, this.waContext.currentTime + this.fadeOutTime);
+            if ((this.waContext!==false) && (!audiotag.paused)) {
+                this.gainNodes[this.IDPlaying].gain.cancelScheduledValues(this.waContext.currentTime);
+                this.gainNodes[this.IDPlaying].gain.setTargetAtTime(0, this.waContext.currentTime, this.fadeOutTime/8.0 );
         
-                var _this = this;
-                (function(atag,prevID){
-                        setTimeout( function(){if (_this.IDPlaying!==prevID) atag.pause();}, _this.fadeOutTime*1000 + 10);
-                })(audiotag,this.IDPlaying);
-                this.IDPlaying = -1;
+                var _this  = this;
+                var prevID = this.IDPlaying;
+                setTimeout( function(){if (_this.IDPlaying!==prevID) audiotag.pause();}, _this.fadeOutTime*1000 + 5);
             } else {
                 audiotag.pause();
-                this.IDPlaying = -1;
             }
-
+            this.IDPlaying = -1;
         }
     }
 
@@ -245,7 +270,17 @@ function clientIsIE() {
 
 // check for Google Chrome/Chromium
 function clientIsChrome() {
-    return !!window.chrome;
+    return !!window.chrome && !clientIsOpera();
+}
+
+// check for Apple Safari
+function clientIsSafari() {
+    return Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+}
+
+// check for Opera
+function clientIsOpera() {
+    return !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
 }
 
 // get date and time formatted as YYYMMDD-hhmmss
